48b546efb19d1c6a046b78f2df88b1f5e9f396ed
insa <insdrame236@gmail.com>
2025-01-03 20:38:19 +0100

    Ajout des fichiers Testvscode-1.ipynb et temp.py

---
+++ temp.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+a= 1
+b = 2
+print(a+b)
+
+
+
+#import pandas as pd
+import numpy as np
+
+def trier_liste(liste):
+    return sorted(liste)
+
+a = 1
+b = 2
+print(a + b)
+print(b)
+
+
+# Exemple d'utilisation de la fonction
+ma_liste = [5, 3, 8, 1, 2]
+print(trier_liste(ma_liste))
